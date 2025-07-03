e95b2baeca370e66a7b122c4ae8a83853325acb0
waltob <benjamin.walton@jax.org>
2025-07-03 14:37:50 -0400

    fix failing test

--- api-clients/src/lib/mvar/mvar.service.spec.ts
+++ api-clients/src/lib/mvar/mvar.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MVarService } from './mvar.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('MVarService', () => {
   let service: MVarService;
+  const mockEnvironment = {
+    unsecuredURLs: {
+      mvar: 'https://mvar.jax.org/mvar'
+    }
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        { provide: 'environment', useValue: mockEnvironment }
+      ]
+    });
     service = TestBed.inject(MVarService);
   });
 
