64a7c11921a3050485c512dac42fe1271c8a8bb4
iimpulse <Michael.Gargano@jax.org>
2025-07-25 15:03:44 -0600

    ontology-search wiring

---
+++ components/src/lib/ontology-search/ontology-search.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ontology-search',
+  imports: [CommonModule],
+  templateUrl: './ontology-search.component.html',
+  styleUrls: ['./ontology-search.component.css'],
+  standalone: true
+})
+export class OntologySearchComponent {
+}
