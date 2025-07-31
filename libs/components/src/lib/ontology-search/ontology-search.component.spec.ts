8f2d64b71f7b788e001b5ef35f568efbad5b81fe
iimpulse <Michael.Gargano@jax.org>
2025-07-31 12:08:57 -0600

    fix ontology search component spec

--- libs/components/src/lib/ontology-search/ontology-search.component.spec.ts
+++ libs/components/src/lib/ontology-search/ontology-search.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { OntologySearchComponent } from './ontology-search.component';
-
+import { OntologyService } from '@jax-data-science/api-clients';
 describe('OntologySearchComponent', () => {
   let component: OntologySearchComponent;
   let fixture: ComponentFixture<OntologySearchComponent>;
@@ -8,6 +8,7 @@ describe('OntologySearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [OntologySearchComponent],
+      providers: [{provide: OntologyService, useValue: {} }] // Mock the OntologyService
     }).compileComponents();
 
     fixture = TestBed.createComponent(OntologySearchComponent);
