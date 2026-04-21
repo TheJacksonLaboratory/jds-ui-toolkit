488e773de01973bcf77ee48512b63063605c96a7
iimpulse <Michael.Gargano@jax.org>
2026-04-21 14:05:03 -0600

    update surface colors

--- libs/themes/src/lib/echo/semantic.ts
+++ libs/themes/src/lib/echo/semantic.ts
@@ -15,23 +15,23 @@ export const semantic = {
   colorScheme: {
     light: {
       surface: {
-        0: '#ffffff',
-        50: '{navy.50}',
-        100: '{navy.100}',
-        200: '{navy.200}',
-        300: '{navy.300}',
-        400: '{navy.400}',
-        500: '{navy.500}',
-        600: '{navy.600}',
-        700: '{navy.700}',
-        800: '{navy.800}',
-        900: '{navy.900}',
-        950: '{navy.950}'
+        0: '{gray.0}',
+        50: '{gray.50}',
+        100: '{gray.100}',
+        200: '{gray.200}',
+        300: '{gray.300}',
+        400: '{gray.400}',
+        500: '{gray.500}',
+        600: '{gray.600}',
+        700: '{gray.700}',
+        800: '{gray.800}',
+        900: '{gray.900}',
+        950: '{gray.950}'
       }
     },
     dark: {
       surface: {
-        0: '#ffffff',
+        0: '{gray.0}',
         50: '{slate.50}',
         100: '{slate.100}',
         200: '{slate.200}',
