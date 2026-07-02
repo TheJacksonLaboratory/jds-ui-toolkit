a0b892cf30f8807933fe9e5aca770273a3cbe0a7
iimpulse <Michael.Gargano@jax.org>
2026-07-02 12:11:07 -0600

    feat: refactor ShowcaseProgressWidget to use DocVariationsComponent

--- apps/demo/src/app/components/pages/progress-widget/showcase-progress-widget.component.ts
+++ apps/demo/src/app/components/pages/progress-widget/showcase-progress-widget.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ProgressWidgetComponent } from '@jax-data-science/components';
+import { AfterViewInit, Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
 import { Button } from 'primeng/button';
+import { ProgressWidgetComponent } from '@jax-data-science/components';
+import { progressWidgetDoc } from '@jax-data-science/component-docs';
+import { DocVariationsComponent } from '../../../docs-shell/tab-content/doc-variations/doc-variations.component';
 
 @Component({
   selector: 'app-showcase-progress-widget',
-  imports: [ProgressWidgetComponent, Button],
+  imports: [DocVariationsComponent, NgTemplateOutlet, ProgressWidgetComponent, Button],
   templateUrl: './showcase-progress-widget.component.html',
   styleUrl: './showcase-progress-widget.component.css',
+  standalone: true,
 })
-export class ShowcaseProgressWidgetComponent implements OnInit, OnDestroy {
+export class ShowcaseProgressWidgetComponent implements OnInit, AfterViewInit, OnDestroy {
+  readonly doc = progressWidgetDoc;
+  demoTemplates = new Map<string, TemplateRef<void>>();
+
   message = 'Loading...';
   isLoading = true;
   blockUi = false;
   unblockMessage = '';
   private intervalId?: ReturnType<typeof setInterval>;
 
-  ngOnInit() {
-    // Simulate a loading process
+  @ViewChild('tplBasicSpinner') tplBasicSpinner!: TemplateRef<void>;
+  @ViewChild('tplWithMessage') tplWithMessage!: TemplateRef<void>;
+  @ViewChild('tplDynamicMessage') tplDynamicMessage!: TemplateRef<void>;
+  @ViewChild('tplWithIcon') tplWithIcon!: TemplateRef<void>;
+  @ViewChild('tplBlockUi') tplBlockUi!: TemplateRef<void>;
+
+  ngOnInit(): void {
     setTimeout(() => {
       this.message = 'Done!';
       this.isLoading = false;
     }, 3000);
   }
 
-  toggleBlockUi() {
+  ngAfterViewInit(): void {
+    this.demoTemplates.set('basic-spinner', this.tplBasicSpinner);
+    this.demoTemplates.set('with-message', this.tplWithMessage);
+    this.demoTemplates.set('dynamic-message', this.tplDynamicMessage);
+    this.demoTemplates.set('with-icon', this.tplWithIcon);
+    this.demoTemplates.set('block-ui', this.tplBlockUi);
+  }
+
+  toggleBlockUi(): void {
     this.blockUi = true;
     this.unblockMessage = 'UI is blocked';
     let countdown = 4;
@@ -31,18 +51,14 @@ export class ShowcaseProgressWidgetComponent implements OnInit, OnDestroy {
       countdown--;
       this.unblockMessage = `Unblocking in ${countdown}...`;
       if (countdown === 0) {
-        if (this.intervalId) {
-          clearInterval(this.intervalId);
-        }
+        clearInterval(this.intervalId);
         this.intervalId = undefined;
         this.blockUi = false;
       }
     }, 1000);
   }
 
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
+  ngOnDestroy(): void {
+    if (this.intervalId) clearInterval(this.intervalId);
   }
 }
